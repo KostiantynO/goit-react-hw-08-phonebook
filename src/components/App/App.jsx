94343d4962ecf14f9ee30e1072ca764bf9efd413
Kostiantyn Ochenash <Ochenashk@gmail.com>
2022-05-12 21:06:19 +0300

    removes buttons from app, they helped to purge persisted state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,13 @@ import { useSetTheme } from 'hooks';
 import { AppRouter } from 'components/AppRouter';
 import { GlobalStyle } from 'components/common';
 import { useDispatch } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { authOperations } from 'redux/auth';
 
 import { ThemeProvider } from 'styled-components';
 import { AppStyled } from './App.styled';
-import { persistor } from 'redux/store';
 
 export const App = () => {
-  const [isPurged, setIsPurged] = useState(false);
   const dispatch = useDispatch();
   const theme = useSetTheme(dispatch);
 
@@ -18,7 +16,7 @@ export const App = () => {
     dispatch(authOperations.refresh());
   }, [dispatch]);
 
-  return isPurged ? (
+  return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyle />
@@ -28,42 +26,5 @@ export const App = () => {
         </AppStyled>
       </>
     </ThemeProvider>
-  ) : (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        gap: 30,
-      }}
-    >
-      <button
-        style={{ padding: 20 }}
-        onClick={async () => {
-          dispatch(authOperations.logOut());
-          await persistor.purge();
-          setIsPurged(true);
-        }}
-      >
-        1. Purge persisted state and logout.
-      </button>
-
-      <button
-        style={{
-          padding: 20,
-          backgroundColor: '#2196f3',
-          color: 'white',
-          '&:hover': {
-            backgroundColor: 'red',
-          },
-        }}
-        onClick={() => {
-          setIsPurged(true);
-        }}
-      >
-        2. Continue as usual (Render App with current state)
-      </button>
-    </div>
   );
 };
